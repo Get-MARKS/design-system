e5f7508abd42dda9390599b7a47306baa767a0df
satyamskillz <satyamskillz@gmail.com>
2023-03-22 15:37:17 +0530

    Update: checkbox component package rebuild

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import React, { ElementType, MouseEventHandler } from 'react';
+import React, { ElementType, MouseEventHandler, ChangeEventHandler } from 'react';
 
 type ButtonType = "primary" | "secondary" | "ghost" | "danger";
 type ButtonSize = "small" | "default" | "medium" | "large";
@@ -15,6 +15,23 @@ interface ButtonProps {
     onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-declare const _default: React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<unknown>>;
+declare const _default$1: React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<unknown>>;
 
-export { _default as Button };
+type checkboxSize = "small" | "default" | "medium" | "large";
+type checkboxType = "fill" | "empty" | "line";
+type labelAlignment = "left" | "right";
+interface CheckboxProps {
+    name: string;
+    label: string;
+    title?: string;
+    value?: string;
+    disabled: boolean;
+    type: checkboxType;
+    size: checkboxSize;
+    labelAlign?: labelAlignment;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+declare const _default: React.ForwardRefExoticComponent<CheckboxProps & React.RefAttributes<unknown>>;
+
+export { _default$1 as Button, _default as Checkbox };
