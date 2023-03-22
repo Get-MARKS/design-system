1f46fdcff89e398de2ae7604fbd0519a61155865
satyamskillz <satyamskillz@gmail.com>
2023-03-22 15:25:09 +0530

    Updated checkbox story and test

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -37,7 +37,7 @@ const gridStyle = {
 	marginBottom: "1rem",
 };
 
-export const Sizes = () => {
+export const allCheckboxes = () => {
 	return (
 		<Fragment>
 			<div style={gridStyle}>
--- src/components/Checkbox/Checkbox.test.tsx
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -1,28 +1,14 @@
 // Generated with util/create-component.js
-  import { render } from "@testing-library/react";
-  import React from "react";
-  
-  import Checkbox from "./Checkbox";
-  import { CheckboxProps } from "./Checkbox.types";
-  
-  describe("Test Component", () => {
-    let props: CheckboxProps;
-  
-    beforeEach(() => {
-      props = {
-        content: "Initial content"
-      };
-    });
-  
-    const renderComponent = () => render(<Checkbox {...props} />);
-  
-    it("should render foo text correctly", () => {
-      props.content = "Satyam was here";
+import { render } from "@testing-library/react";
+import React from "react";
 
-      const { getByTestId } = renderComponent(); 
-      const component = getByTestId("Checkbox");
-  
-      expect(component).toHaveTextContent("Satyam was here");
-    });
-  });
-  
+import Checkbox from "./Checkbox";
+import { CheckboxProps } from "./Checkbox.types";
+
+describe("Checkbox", () => {
+	test("renders the Button component", () => {
+		render(
+			<Checkbox name="test" type="line" size="small" disabled={false} label="Test Checkbox" />
+		);
+	});
+});
