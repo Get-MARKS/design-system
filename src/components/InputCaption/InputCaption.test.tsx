f138f0b4cff7e8047ae6d44dd0703c6e6e71c30b
satyamskillz <satyamskillz@gmail.com>
2023-03-29 18:57:42 +0530

    [Pending] Added Input Caption component

---
+++ src/components/InputCaption/InputCaption.test.tsx
@@ -0,0 +1,13 @@
+// Generated with util/create-component.js
+  import { render } from "@testing-library/react";
+  import React from "react";
+  
+  import InputCaption from "./InputCaption";
+  import { InputCaptionProps } from "./InputCaption.types";
+  
+  describe("InputCaption", () => {
+    test("renders the InputCaption component", () => {
+      render(<InputCaption />);
+    });
+  });
+  
---
+++ src/components/InputCaption/InputCaption.types.ts
@@ -0,0 +1,7 @@
+// Generated with util/create-component.js
+export interface InputCaptionProps {
+	type: "default" | "success" | "error" | "info" | "password" | "passwordState";
+	size: "default" | "small" | "medium" | "large";
+	passwordState: number;
+	content: string;
+}
